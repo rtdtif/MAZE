d3225c2b830f4af557f8a6187f2ed302c75a4c8d
rtdtif <rtd.tif@gmail.com>
2018-10-24 18:12:29 +0300

    Added showMenu function.

--- MAZE/MAZE/main.cpp
+++ MAZE/MAZE/main.cpp
@@ -1,5 +1,30 @@
-#include <iostream>
+﻿#include <iostream>
 #include <array>
+#include <conio.h>
 
 
+const int maxRowsCount = 25;
+const int maxColumnsCount = 35;
 
+const char mapPlayerIcon = '@';
+const char mapExitIcon = 'E';
+const char mapWallIcon = '#';
+const char mapFieldIcon = ' ';
+
+using Map = std::array < std::array<char, maxRowsCount>, maxColumnsCount>;
+
+
+void showMenu()
+{
+	std::cout << "\t ~***  MAZE  ***~" << std::endl;
+	std::cout << std::endl;
+	std::cout << "Tip : use arrows to control your personage." << std::endl;
+	std::cout << std::endl;
+}
+
+int main()
+{
+	showMenu();
+	getchar();
+	return 0;
+}
