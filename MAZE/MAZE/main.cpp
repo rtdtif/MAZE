10853680bbef676045c214047b540d8b1a12c6c4
rtdtif <rtd.tif@gmail.com>
2018-10-24 19:16:17 +0300

    Added genereteMap function.

--- MAZE/MAZE/main.cpp
+++ MAZE/MAZE/main.cpp
@@ -5,11 +5,13 @@
 
 const int maxRowsCount = 25;
 const int maxColumnsCount = 50;
+const int exitRetreat = 4;
 
 const char mapPlayerIcon = '@';
 const char mapExitIcon = 'E';
 const char mapWallIcon = '#';
 const char mapFieldIcon = ' ';
+const char mapTestIcon = 't';
 
 using Map = std::array < std::array<char, maxColumnsCount>, maxRowsCount>;
 
@@ -17,12 +19,12 @@ using Map = std::array < std::array<char, maxColumnsCount>, maxRowsCount>;
 void showMenu()
 {
 	std::cout << std::endl;
-	std::cout << "\t\t ~***  MAZE  ***~" << std::endl;
-	std::cout << std::endl;
+	std::cout << "\t\t      ~***  MAZE  ***~" << std::endl;
+	std::cout << "\tMain goal: Help dog to find exit." << std::endl;
 	std::cout << "\tTip : use arrows to control your personage." << std::endl;
 	std::cout << std::endl;
 }
-void fillMap(Map &map)
+void fillMapBorders(Map &map)
 {
 	for (int row = 0; row < maxRowsCount; ++row)
 	{
@@ -36,7 +38,32 @@ void fillMap(Map &map)
 			{
 				map[row][column] = mapFieldIcon;
 			}
+			if (row == maxRowsCount - exitRetreat && column == maxColumnsCount - exitRetreat*2)
+			{
+				map[row][column] = mapExitIcon;
+			}
+			if (row == exitRetreat && column == exitRetreat * 2)
+			{
+				map[row][column] = mapPlayerIcon;
+			}
 		}
+	}
+}
+void genereteMap(Map &map)
+{
+	for (int row = 1; row < maxRowsCount-1; ++row)
+	{
+		for (int column = 1; column < maxColumnsCount-1; ++column)
+		{
+			if ((rand() % 5 == 1) && (row != maxRowsCount - exitRetreat && column != maxColumnsCount - exitRetreat * 2))
+			{
+				if(row != exitRetreat && column != exitRetreat * 2)
+				{
+					map[row][column] = mapWallIcon;
+				}
+			}
+		}
+		
 	}
 }
 void showMap(const Map &map)
@@ -56,7 +83,8 @@ int main()
 {
 	Map map;
 	showMenu();
-	fillMap(map);
+	fillMapBorders(map);
+	genereteMap(map);
 	showMap(map);
 	getchar();
 	return 0;
